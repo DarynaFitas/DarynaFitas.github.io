243f740c576f0deaab399ab7e3b899f4cc1387c5
DarynaFitas <128675995+DarynaFitas@users.noreply.github.com>
2023-12-19 19:34:24 +0200

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+	static void Main()
+	{
+		using (var dbKafedra = new Kafedra())
+		{
+			Console.WriteLine("Введіть значення параметра (surname):");
+			string userInput = Console.ReadLine();
+
+			var teacherBySurname = dbKafedra.Kafedra.FirstOrDefault(t => t.Surname == userInput);
+
+			if (teacherBySurname != null)
+			{
+				Console.WriteLine($"Знайдено викладача: {teacherBySurname.FirstName} {teacherBySurname.LastName}");
+			}
+			else
+			{
+				Console.WriteLine("Викладача з таким прізвищем не знайдено.");
+			}
+		}
+	}
+}
